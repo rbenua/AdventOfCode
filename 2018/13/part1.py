40622665d974078dd9a0373376e83307fa7a7ad1
Rick Benua <rick@benua.net>
2018-12-14 04:15:47 -0800

    day 13

---
+++ 2018/13/part1.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+
+import sys
+import enum
+
+class Track(enum.Enum):
+    none = 0
+    up_down = enum.auto()
+    left_right = enum.auto()
+    up_left = enum.auto()
+    down_right = enum.auto()
+    down_left = enum.auto()
+    up_right = enum.auto()
+    cross = enum.auto()
+
+class CartDir(enum.Enum):
+    up = 0
+    right = 1
+    down = 2
+    left = 3
+
+map = []
+carts = {}
+
+def read_input(filename):
+    with open(filename, 'r') as f:
+        y = 0
+        for line in f:
+            l = []
+            for x in range(len(line)):
+                if line[x] == ' ' or line[x] == '\n':
+                    l.append(Track.none)
+                elif line[x] == '|':
+                    l.append(Track.up_down)
+                elif line[x] == '-':
+                    l.append(Track.left_right)
+                elif line[x] == '/':
+                    if line[x+1] == '-' or line[x+1] == '+':
+                        l.append(Track.down_right)
+                    else:
+                        l.append(Track.up_left)
+                elif line[x] == '\\':
+                    if line[x+1] == '-' or line[x+1] == '+':
+                        l.append(Track.up_right)
+                    else:
+                        l.append(Track.down_left)
+                elif line[x] == '+':
+                    l.append(Track.cross)
+                elif line[x] == '^':
+                    l.append(Track.up_down)
+                    carts[(y,x)] = (CartDir.up, 0)
+                elif line[x] == 'v':
+                    l.append(Track.up_down)
+                    carts[(y,x)] = (CartDir.down, 0)
+                elif line[x] == '>':
+                    l.append(Track.left_right)
+                    carts[(y,x)] = (CartDir.right, 0)
+                elif line[x] == '<':
+                    l.append(Track.left_right)
+                    carts[(y,x)] = (CartDir.left, 0)
+            y += 1
+            map.append(l)
+
+def step():
+    for ((y,x),(d,i)) in sorted(carts.items()):
+        if d == CartDir.up:
+            newpos = (y-1,x)
+        elif d == CartDir.down:
+            newpos = (y+1,x)
+        elif d == CartDir.left:
+            newpos = (y,x-1)
+        else:
+            newpos = (y,x+1)
+        if newpos in carts:
+            return newpos
+        
+        dt = map[newpos[0]][newpos[1]]
+        newi = i
+        if dt == Track.up_down or dt == Track.left_right:
+            newdir = d
+        elif (dt == Track.up_left and d == CartDir.down) or (dt == Track.down_left and d == CartDir.up):
+            newdir = CartDir.left
+        elif (dt == Track.up_right and d == CartDir.down) or (dt == Track.down_right and d == CartDir.up):
+            newdir = CartDir.right
+        elif (dt == Track.up_left and d == CartDir.right) or (dt == Track.up_right and d == CartDir.left):
+            newdir = CartDir.up
+        elif (dt == Track.down_left and d == CartDir.right) or (dt == Track.down_right and d == CartDir.left):
+            newdir = CartDir.down
+        elif dt == Track.cross:
+            if i == 0:
+                newdir = CartDir((d.value - 1) % 4)
+            elif i == 1:
+                newdir = d
+            elif i == 2:
+                newdir = CartDir((d.value + 1) % 4)
+            newi = (i + 1) % 3
+        else:
+            print("Derailed",(x,y),"=>",newpos)
+            exit(1)
+
+        del carts[(y,x)]
+        carts[newpos] = (newdir, newi)
+    return None
+
+if __name__ == "__main__":
+    read_input(sys.argv[1])
+    while True:
+        p = step()
+        if p is not None:
+            print(p)
+            exit(0)
---
+++ 2018/13/part2.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python3
+
+import sys
+import enum
+
+class Track(enum.Enum):
+    none = 0
+    up_down = enum.auto()
+    left_right = enum.auto()
+    up_left = enum.auto()
+    down_right = enum.auto()
+    down_left = enum.auto()
+    up_right = enum.auto()
+    cross = enum.auto()
+
+class CartDir(enum.Enum):
+    up = 0
+    right = 1
+    down = 2
+    left = 3
+
+map = []
+carts = {}
+
+def read_input(filename):
+    with open(filename, 'r') as f:
+        y = 0
+        for line in f:
+            l = []
+            for x in range(len(line)):
+                if line[x] == ' ' or line[x] == '\n':
+                    l.append(Track.none)
+                elif line[x] == '|':
+                    l.append(Track.up_down)
+                elif line[x] == '-':
+                    l.append(Track.left_right)
+                elif line[x] == '/':
+                    if line[x+1] in '-<>+':
+                        l.append(Track.down_right)
+                    else:
+                        l.append(Track.up_left)
+                elif line[x] == '\\':
+                    if line[x+1] in '-<>+':
+                        l.append(Track.up_right)
+                    else:
+                        l.append(Track.down_left)
+                elif line[x] == '+':
+                    l.append(Track.cross)
+                elif line[x] == '^':
+                    l.append(Track.up_down)
+                    carts[(y,x)] = (CartDir.up, 0)
+                elif line[x] == 'v':
+                    l.append(Track.up_down)
+                    carts[(y,x)] = (CartDir.down, 0)
+                elif line[x] == '>':
+                    l.append(Track.left_right)
+                    carts[(y,x)] = (CartDir.right, 0)
+                elif line[x] == '<':
+                    l.append(Track.left_right)
+                    carts[(y,x)] = (CartDir.left, 0)
+            y += 1
+            map.append(l)
+
+def step():
+    it = sorted(carts.items())
+    idx = 0
+    while idx < len(it):
+        ((y,x),(d,i)) = it[idx]
+        if d == CartDir.up:
+            newpos = (y-1,x)
+        elif d == CartDir.down:
+            newpos = (y+1,x)
+        elif d == CartDir.left:
+            newpos = (y,x-1)
+        else:
+            newpos = (y,x+1)
+        if newpos in carts:
+            if newpos > (y,x):
+                try:
+                    it.remove((newpos,carts[newpos]))
+                except ValueError:
+                    pass
+            del carts[(y,x)]
+            del carts[newpos]
+            idx += 1
+            continue
+        
+        dt = map[newpos[0]][newpos[1]]
+        newi = i
+        if dt == Track.up_down or dt == Track.left_right:
+            newdir = d
+        elif (dt == Track.up_left and d == CartDir.down) or (dt == Track.down_left and d == CartDir.up):
+            newdir = CartDir.left
+        elif (dt == Track.up_right and d == CartDir.down) or (dt == Track.down_right and d == CartDir.up):
+            newdir = CartDir.right
+        elif (dt == Track.up_left and d == CartDir.right) or (dt == Track.up_right and d == CartDir.left):
+            newdir = CartDir.up
+        elif (dt == Track.down_left and d == CartDir.right) or (dt == Track.down_right and d == CartDir.left):
+            newdir = CartDir.down
+        elif dt == Track.cross:
+            if i == 0:
+                newdir = CartDir((d.value - 1) % 4)
+            elif i == 1:
+                newdir = d
+            elif i == 2:
+                newdir = CartDir((d.value + 1) % 4)
+            newi = (i + 1) % 3
+        else:
+            print("Derailed",(y,x),"=>",newpos)
+            raise Error()
+
+        del carts[(y,x)]
+        carts[newpos] = (newdir, newi)
+        idx += 1
+
+    if len(carts) == 1:
+        return carts.items()
+    return None
+
+if __name__ == "__main__":
+    read_input(sys.argv[1])
+    while True:
+        p = step()
+        if p is not None:
+            print(p)
+            exit(0)
