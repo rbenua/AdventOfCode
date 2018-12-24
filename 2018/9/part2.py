8748f35e123746370fd261cd0060da3614174d43
Rick Benua <rick@benua.net>
2018-12-24 16:18:09 -0500

    doubly linked list solution for 9b

--- 2018/9/part2.py
+++ 2018/9/part2.py
@@ -2,24 +2,64 @@
 
 import sys
 
+class Node:
+    def __init__(self, value):
+        self.prev = self
+        self.next = self
+        self.value = value
+
+    def insert(self, value):
+        new = Node(value)
+        new.prev = self
+        new.next = self.next
+        self.next.prev = new
+        self.next = new
+        return new
+    
+    def remove(self):
+        self.next.prev = self.prev
+        self.prev.next = self.next
+        return (self.next, self.value)
+
+    def fseek(self, i):
+        if i == 0:
+            return self
+        return self.next.fseek(i-1)
+
+    def bseek(self, i):
+        if i == 0:
+            return self
+        return self.prev.bseek(i-1)
+
 players = int(sys.argv[1])
-marbles = int(sys.argv[2]) * 100 + 1
+marbles = int(sys.argv[2]) + 1
 
 scores = [0] * players
 
-ring = [0]
-curr = 0
+curr = Node(0)
+zero = curr
+
+def printlist():
+    n = zero
+    print("{:2}{}".format(n.value, (" ","*")[n == curr]), end="")
+    n = n.next
+    while n != zero:
+        print("{:2}{}".format(n.value, (" ","*")[n == curr]), end="")
+        n = n.next
+    print()
 
 for marble in range(1, marbles):
+    if marble % 1000 == 0:
+        print("processed {} marbles".format(marble))
     player = marble % players
-    #if marble == marbles - 1:
-    #    marble = marble * 100
     if marble % 23 == 0:
-        curr = (curr - 7) % len(ring)
-        removed = ring.pop(curr)
+        curr = curr.bseek(7)
+        (curr, removed) = curr.remove()
         scores[player] += removed + marble
+        #print("player {} kept {} and removed {}, total score {}".format(player, marble, removed, scores[player]))
     else:
-        curr = (curr + 2) % len(ring)
-        ring.insert(curr, marble)
+        curr = curr.fseek(1)
+        curr = curr.insert(marble)
+    #printlist()
 
 print(max(scores))
