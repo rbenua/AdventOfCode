b6e2ab78c2ef3b84c12df4a4c4ec9f24275a7645
Rick Benua <rick@benua.net>
2019-12-04 22:32:27 -0800

    extensibility cleanup for intcode interpreter

--- 2019/5/day5.py
+++ 2019/5/day5.py
@@ -7,60 +7,78 @@ def read_input(filename):
         return [int(l) for l in f.read().split(",")]
 
 
-def get_input():
+def get_input_1():
+    return 1
+def get_input_5():
     return 5
 
-def set_output(i):
+def out_print(i):
     print("output:", i)
 
-def get_arg(inp, mode, arg):
-    if mode == 0:
-        return inp[arg]
-    elif mode == 1:
-        return arg
-    
-def execute(inp):
+def execute(inp, in_fn, out_fn = out_print):
     pc = 0
     while True:
+        if pc < 0 or pc >= len(inp):
+            print("PC out of bounds", pc)
+            return
         opcode = inp[pc] % 100
         modes = [int(c) for c in list("{:05}".format(int(inp[pc] / 100)))]
         modes.reverse()
-        print(pc, inp[pc], opcode, modes)
-        if pc < 0 or pc >= len(inp) or opcode == 99:
+        def get_arg(offset):
+            mode = modes[offset]
+            arg = inp[pc + offset + 1]
+            if mode == 0:
+                return inp[arg]
+            elif mode == 1:
+                return arg
+            else:
+                print("Invalid input mode", mode)
+                return 0
+        
+        def set_arg(offset, val):
+            mode = modes[offset]
+            arg = inp[pc + offset + 1]
+            if mode == 0:
+                inp[arg] = val
+            else:
+                print("Invalid output mode", mode)
+                return 0
+
+        if opcode == 99:
             return
         elif opcode == 1:
-            inp[inp[pc+3]] = get_arg(inp, modes[0], inp[pc+1]) + get_arg(inp, modes[1], inp[pc+2])
+            set_arg(2, get_arg(0) + get_arg(1))
             pc += 4
         elif opcode == 2:
-            inp[inp[pc+3]] = get_arg(inp, modes[0], inp[pc+1]) * get_arg(inp, modes[1], inp[pc+2])
+            set_arg(2, get_arg(0) * get_arg(1))
             pc += 4
         elif opcode == 3:
-            inp[inp[pc+1]] = get_input()
+            set_arg(0, in_fn())
             pc += 2
         elif opcode == 4:
-            set_output(get_arg(inp, modes[0], inp[pc+1]))
+            out_fn(get_arg(0))
             pc += 2
         elif opcode == 5:
-            if get_arg(inp, modes[0], inp[pc+1]) != 0:
-                pc = get_arg(inp, modes[1], inp[pc+2])
+            if get_arg(0) != 0:
+                pc = get_arg(1)
             else:
                 pc += 3
         elif opcode == 6:
-            if get_arg(inp, modes[0], inp[pc+1]) == 0:
-                pc = get_arg(inp, modes[1], inp[pc+2])
+            if get_arg(0) == 0:
+                pc = get_arg(1)
             else:
                 pc += 3
         elif opcode == 7:
-            if get_arg(inp, modes[0], inp[pc+1]) < get_arg(inp, modes[1], inp[pc+2]):
-                inp[inp[pc+3]] = 1
+            if get_arg(0) < get_arg(1):
+                set_arg(2, 1)
             else:
-                inp[inp[pc+3]] = 0
+                set_arg(2, 0)
             pc += 4
         elif opcode == 8:
-            if get_arg(inp, modes[0], inp[pc+1]) == get_arg(inp, modes[1], inp[pc+2]):
-                inp[inp[pc+3]] = 1
+            if get_arg(0) == get_arg(1):
+                set_arg(2, 1)
             else:
-                inp[inp[pc+3]] = 0
+                set_arg(2, 0)
             pc += 4
 
         else:
@@ -69,16 +87,14 @@ def execute(inp):
             
         
 def part1(inp):
-    execute(inp)
-    pass
+    execute(inp.copy(), get_input_1)
 
 def part2(inp):
-    pass
+    execute(inp.copy(), get_input_5)
 
 def run(filename):
     inp = read_input(filename)
     print(part1(inp))
-    inp = read_input(filename)
     print(part2(inp))
 
 if __name__ == "__main__":
