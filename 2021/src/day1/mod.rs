12b16b95d6860c27b3778d02d6f9b9ab32b39adc
Rick Benua <rick@benua.net>
2021-12-01 22:07:25 -0800

    2021 days 1 and 2

--- 2021/src/day1/mod.rs
+++ 2021/src/day1/mod.rs
@@ -5,20 +5,42 @@ use regex::Regex;
 extern crate string_error;
 use string_error::new_err;
 use parse_display::{Display, FromStr};
+use std::collections::VecDeque;
 
 pub struct Day1{
-    placeholder: i64,
+    depths: Vec<i64>,
 }
 
 pub fn setup(_input:&str) -> Result<Day1, Box<dyn Error>>{
-    Err(new_err("Unimplemented"))
+    let mut depths = Vec::new();
+    for line_opt in read_lines(_input) {
+        let line = line_opt?;
+        depths.push(line.trim().parse::<i64>()?);
+    }
+    Ok(Day1{
+        depths,
+    })
 }
 
 impl Problem for Day1{
     fn part1(&mut self, _input:&str) -> Result<String, Box<dyn Error>>{
-        Ok("".to_string())
+        let mut prev = i64::MAX;
+        let mut total = 0;
+        for cur in &self.depths {
+            if *cur > prev {
+                total += 1;
+            }
+            prev = *cur;
+        }
+        Ok(total.to_string())
     }
     fn part2(&mut self, _input:&str) -> Result<String, Box<dyn Error>>{
-        Ok("".to_string())
+        let mut total : u64 = 0;
+        for i in 3..self.depths.len() {
+            if self.depths[i] > self.depths[i-3] {
+                total += 1;
+            }
+        }
+        Ok(total.to_string())
     }
 }
--- 2021/src/day2/mod.rs
+++ 2021/src/day2/mod.rs
@@ -6,19 +6,80 @@ extern crate string_error;
 use string_error::new_err;
 use parse_display::{Display, FromStr};
 
+enum Command{
+    Forward(i64),
+    Up(i64),
+    Down(i64),
+}
+
+struct State{
+    hpos: i64,
+    depth: i64,
+    aim: i64,
+}
+
 pub struct Day2{
-    placeholder: i64,
+    commands: Vec<Command>,
 }
 
 pub fn setup(_input:&str) -> Result<Day2, Box<dyn Error>>{
-    Err(new_err("Unimplemented"))
+    let mut res = Day2{
+        commands: Vec::new(),
+    };
+    for line in read_lines(_input){
+        let line_opt = line?;
+        let mut sl = line_opt.split(" ");
+        let cmd = sl.next().unwrap();
+        let amt = sl.next().unwrap().parse::<i64>()?;
+        res.commands.push(match cmd {
+            "forward" => Command::Forward(amt),
+            "up" => Command::Up(amt),
+            "down" => Command::Down(amt),
+            _ => return Err(new_err("bad command")),
+        });
+    }
+    Ok(res)
 }
 
+fn step1(cmd: &Command, st: &mut State) {
+    match cmd {
+        Command::Forward(amt) => st.hpos += amt,
+        Command::Up(amt) => st.depth -= amt,
+        Command::Down(amt) => st.depth += amt,
+    }
+}
+
+fn step2(cmd: &Command, st: &mut State) {
+    match cmd {
+        Command::Forward(amt) => {
+            st.hpos += amt;
+            st.depth += amt * st.aim;
+        },
+        Command::Up(amt) => st.aim -= amt,
+        Command::Down(amt) => st.aim += amt,
+    }
+}
 impl Problem for Day2{
     fn part1(&mut self, _input:&str) -> Result<String, Box<dyn Error>>{
-        Ok("".to_string())
+        let mut st = State{
+            hpos: 0,
+            depth: 0,
+            aim: 0,
+        };
+        for cmd in &self.commands {
+            step1(&cmd, &mut st);
+        }
+        Ok((st.hpos * st.depth).to_string())
     }
     fn part2(&mut self, _input:&str) -> Result<String, Box<dyn Error>>{
-        Ok("".to_string())
+        let mut st = State{
+            hpos: 0,
+            depth: 0,
+            aim: 0,
+        };
+        for cmd in &self.commands {
+            step2(&cmd, &mut st);
+        }
+        Ok((st.hpos * st.depth).to_string())
     }
 }
