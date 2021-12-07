7b2b12bf623768eccfcaec99cc39ddfb661d493d
Rick Benua <rick@benua.net>
2021-12-06 21:57:35 -0800

    day 7

--- 2021/src/day7/mod.rs
+++ 2021/src/day7/mod.rs
@@ -7,18 +7,37 @@ use string_error::new_err;
 use parse_display::{Display, FromStr};
 
 pub struct Day7{
-    placeholder: i64,
+    positions: Vec<i64>,
 }
 
 pub fn setup(_input:&str) -> Result<Day7, Box<dyn Error>>{
-    Err(new_err("Unimplemented"))
+    let line = read_lines(_input).next().unwrap()?;
+    let positions = line.split(',').map(|x|{x.trim().parse().unwrap()}).collect();
+    Ok(Day7{positions})
+}
+
+fn score1(test: i64, positions: &Vec<i64>) -> i64 {
+    positions.iter().map(|p|{i64::abs(p - test)}).sum()
+}
+
+fn score2(test: i64, positions: &Vec<i64>) -> i64 {
+    positions.iter().map(|p|{
+        let d = i64::abs(p - test);
+        (d * (d + 1)) / 2
+    }).sum()
+}
+
+fn search(positions: &Vec<i64>, score: fn(i64, &Vec<i64>) -> i64) -> i64 {
+    let start = *positions.iter().min().unwrap();
+    let end = *positions.iter().max().unwrap();
+    ((start+1)..end).map(|p|{score(p, positions)}).min().unwrap()
 }
 
 impl Problem for Day7{
     fn part1(&mut self, _input:&str) -> Result<String, Box<dyn Error>>{
-        Ok("".to_string())
+        Ok(search(&self.positions, score1).to_string())
     }
     fn part2(&mut self, _input:&str) -> Result<String, Box<dyn Error>>{
-        Ok("".to_string())
+        Ok(search(&self.positions, score2).to_string())
     }
 }
