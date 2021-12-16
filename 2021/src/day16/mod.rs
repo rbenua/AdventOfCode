841a4217efba5fd7286206f465406b0eb1b5aebc
Rick Benua <rick@benua.net>
2021-12-16 01:50:20 -0800

    refactor day 16, add an ast

--- 2021/src/day16/mod.rs
+++ 2021/src/day16/mod.rs
@@ -8,6 +8,20 @@ use parse_display::{Display, FromStr};
 
 pub struct Day16{
     bits: Vec<bool>,
+    parsed: Packet,
+}
+
+#[derive(Debug)]
+struct Packet{
+    version: u64,
+    ptype: u64,
+    contents: Contents,
+}
+
+#[derive(Debug)]
+enum Contents{
+    Int(u64),
+    Oper(Vec<Packet>),
 }
 
 pub fn setup(_input:&str) -> Result<Day16, Box<dyn Error>>{
@@ -19,7 +33,7 @@ pub fn setup(_input:&str) -> Result<Day16, Box<dyn Error>>{
             bits.push((i & (1 << d)) > 0);
         }
     }
-    Ok(Day16{bits})
+    Ok(Day16{parsed: parse(&bits).0, bits})
 }
 
 fn to_int(bits: &[bool]) -> u64 {
@@ -33,120 +47,82 @@ fn to_int(bits: &[bool]) -> u64 {
     res
 }
 
-fn sum_versions(bits: &[bool]) -> (u64, usize) {
-    //println!("parsing packet ({}): {:?}", bits.len(), bits);
-    let mut total = to_int(&bits[0..3]);
-    let mut ptype = to_int(&bits[3..6]);
+fn parse(bits: &[bool]) -> (Packet, usize) {
+    let version = to_int(&bits[0..3]);
+    let ptype = to_int(&bits[3..6]);
     let mut consumed = 6;
-    if ptype == 4 {
-        let mut i = 6;
-        while bits[i] {
-            i += 5;
+    let contents = if ptype == 4 {
+        let mut val = 0;
+        while bits[consumed] {
+            val <<= 4;
+            val += to_int(&bits[consumed+1..consumed+5]);
             consumed += 5;
         }
+        val <<= 4;
+        val += to_int(&bits[consumed+1..consumed+5]);
         consumed += 5;
-        //println!("int of length {}", consumed);
+        Contents::Int(val)
     }
     else {
+        let mut res = Vec::new();
         if !bits[6] {
             let rem_length = to_int(&bits[7..22]) as usize;
             let mut start = 22;
             let end = 22 + rem_length;
-            //println!("mode 0, remaining length {}", rem_length);
             consumed = end;
             while start < end {
-                let (t, c) = sum_versions(&bits[start..end]);
-                total += t;
+                let (t, c) = parse(&bits[start..end]);
+                res.push(t);
                 start += c;
             }
         }
         else {
             let rem_packets = to_int(&bits[7..18]);
-            //println!("mode 1, remaining packets {}", rem_packets);
             let mut start = 18;
             for _ in 0..rem_packets {
-                let (t, c) = sum_versions(&bits[start..]);
-                total += t;
+                let (t, c) = parse(&bits[start..]);
+                res.push(t);
                 start += c;
             }
             consumed = start;
         }
-    }
-    //println!("total {}, consumed {}", total, consumed);
-    (total, consumed) 
+        Contents::Oper(res)
+    };
+    (Packet{version, ptype, contents}, consumed)
 }
 
-fn eval_subpackets(ptype: u64, subpackets: &Vec<u64>) -> u64 {
-    //println!("type {}, sub {:?}", ptype, subpackets);
+fn sum_versions(packet: &Packet) -> u64 {
+    packet.version + match &packet.contents {
+        Contents::Int(_) => 0,
+        Contents::Oper(v) => v.iter().map(sum_versions).sum(),
+    }
+}
+
+fn eval(packet: &Packet) -> u64 {
+    match &packet.contents {
+        Contents::Int(x) => *x,
+        Contents::Oper(v) => eval_subpackets(packet.ptype, &mut v.iter().map(eval))
+    }
+}
+
+fn eval_subpackets(ptype: u64, subpackets: &mut impl Iterator<Item=u64>) -> u64 {
     match ptype {
-        0 => subpackets.iter().sum(),
-        1 => subpackets.iter().product(),
-        2 => *subpackets.iter().min().unwrap(),
-        3 => *subpackets.iter().max().unwrap(),
-        5 => if subpackets[0] > subpackets[1] {1} else {0},
-        6 => if subpackets[0] < subpackets[1] {1} else {0},
-        7 => if subpackets[0] == subpackets[1] {1} else {0},
+        0 => subpackets.sum(),
+        1 => subpackets.product(),
+        2 => subpackets.min().unwrap(),
+        3 => subpackets.max().unwrap(),
+        5 => if subpackets.next().unwrap() > subpackets.next().unwrap() {1} else {0},
+        6 => if subpackets.next().unwrap() < subpackets.next().unwrap() {1} else {0},
+        7 => if subpackets.next().unwrap() == subpackets.next().unwrap() {1} else {0},
         _ => panic!("bad packet type {}", ptype),
     }
 }
 
-fn eval(bits: &[bool]) -> (u64, usize) {
-    //println!("parsing packet ({})", bits.len());
-    let mut total = 0;
-    let mut ptype = to_int(&bits[3..6]);
-    let mut consumed = 6;
-    if ptype == 4 {
-        let mut i = 6;
-        while bits[i] {
-            total = total << 4;
-            total += to_int(&bits[i+1..i+5]);
-            //println!("{}", total);
-            i += 5;
-            consumed += 5;
-        }
-        total = total << 4;
-        total += to_int(&bits[i+1..i+5]);
-        consumed += 5;
-        //println!("int of length {}", consumed);
-    }
-    else {
-        if !bits[6] {
-            let rem_length = to_int(&bits[7..22]) as usize;
-            let mut start = 22;
-            let end = 22 + rem_length;
-            //println!("mode 0, remaining length {}", rem_length);
-            consumed = end;
-            let mut sub_results = Vec::new();
-            while start < end {
-                let (t, c) = eval(&bits[start..end]);
-                sub_results.push(t);
-                start += c;
-            }
-            total = eval_subpackets(ptype, &sub_results);
-        }
-        else {
-            let rem_packets = to_int(&bits[7..18]);
-            //println!("mode 1, remaining packets {}", rem_packets);
-            let mut start = 18;
-            let mut sub_results = Vec::new();
-            for _ in 0..rem_packets {
-                let (t, c) = eval(&bits[start..]);
-                sub_results.push(t);
-                start += c;
-            }
-            total = eval_subpackets(ptype, &sub_results);
-            consumed = start;
-        }
-    }
-    //println!("total {}, consumed {}", total, consumed);
-    (total, consumed) 
-}
-
 impl Problem for Day16{
     fn part1(&mut self, _input:&str) -> Result<String, Box<dyn Error>>{
-        Ok(sum_versions(&self.bits).0.to_string())
+        Ok(sum_versions(&self.parsed).to_string())
     }
     fn part2(&mut self, _input:&str) -> Result<String, Box<dyn Error>>{
-        Ok(eval(&self.bits).0.to_string())
+        Ok(eval(&self.parsed).to_string())
     }
 }
