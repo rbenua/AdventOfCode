549b4ad2665eb57eb55f0f570b9a2daa88b3a8c5
Rick Benua <rick@benua.net>
2021-12-19 22:29:28 -0800

    day 20

--- 2021/src/day20/mod.rs
+++ 2021/src/day20/mod.rs
@@ -5,20 +5,125 @@ use regex::Regex;
 extern crate string_error;
 use string_error::new_err;
 use parse_display::{Display, FromStr};
+use std::collections::{HashMap, HashSet};
+
 
 pub struct Day20{
-    placeholder: i64,
+    conv: Vec<bool>,
+    input_img: Image,
+}
+
+#[derive(Debug, Clone)]
+struct Image{
+    known: HashMap<(i64, i64), bool>,
+    default: bool,
+}
+
+fn nbrs((x, y): (i64, i64)) -> [(i64, i64); 9] {
+    [(x-1, y-1), (x, y-1), (x+1, y-1),
+     (x-1, y),   (x, y),   (x+1, y),
+     (x-1, y+1), (x, y+1), (x+1, y+1)]
 }
 
 pub fn setup(_input:&str) -> Result<Day20, Box<dyn Error>>{
-    Err(new_err("Unimplemented"))
+    let mut lines = read_lines(_input);
+    let first_line = lines.next().unwrap()?;
+    let conv = first_line.chars().map(|c|c == '#').collect();
+    lines.next();
+    let mut input_img = Image{
+        known: HashMap::new(),
+        default: false,
+    };
+    let mut y = 0;
+    for line_opt in lines {
+        let mut x = 0;
+        for c in line_opt?.chars() {
+            input_img.known.insert((x, y), c == '#');
+            x += 1;
+        }
+        y += 1;
+    }
+    Ok(Day20{conv, input_img})
+}
+
+fn step(conv: &Vec<bool>, input_img: &Image) -> Image {
+    let mut res = HashMap::new();
+    let new_default = conv[if input_img.default {511} else {0}];
+
+    for p in input_img.known.keys().map(|pp|nbrs(*pp)).flatten().collect::<HashSet<(i64,i64)>>() {
+        let mut idx = 0;
+        for n in nbrs(p) {
+            idx <<= 1;
+            idx += if *input_img.known.get(&n).unwrap_or(&input_img.default) {1} else {0};
+        }
+        if (idx == 0 && !input_img.default) || (idx == 511 && input_img.default) {
+            continue;
+        }
+        res.insert(p, conv[idx as usize]);
+    }
+
+    Image{
+        known: res,
+        default: new_default,
+    }
+}
+
+fn ch(i: bool) -> char {
+    if i {'#'} else {'.'}
+}
+fn printgrid(img: &Image) {
+    let mut ymin = 0;
+    let mut ymax = 0;
+    let mut xmin = 0;
+    let mut xmax = 0;
+    for (x, y) in img.known.keys() {
+        if *x > xmax {
+            xmax = *x;
+        }
+        if *x < xmin {
+            xmin = *x;
+        }
+        if *y > ymax {
+            ymax = *y;
+        }
+        if *y < ymin {
+            ymin = *y;
+        }
+    } 
+    println!("default: {}, x={}..{}, y={}..{}", ch(img.default), xmin, xmax, ymin, ymax);
+
+    for y in ymin..(ymax+1) {
+        println!("{}", (xmin..(xmax+1)).map(|x|{
+            ch(*img.known.get(&(x, y)).unwrap_or(&img.default))
+        }).collect::<String>());
+    }
 }
 
 impl Problem for Day20{
     fn part1(&mut self, _input:&str) -> Result<String, Box<dyn Error>>{
-        Ok("".to_string())
+        let mut img = self.input_img.clone();
+        //printgrid(&img);
+        for _ in 0..2 {
+            img = step(&self.conv, &img);
+            //printgrid(&img);
+        }
+        if img.default {
+            return Err(new_err("infinite result"));
+        }
+        let res = img.known.values().filter(|x|**x).count();
+        Ok(res.to_string())
     }
     fn part2(&mut self, _input:&str) -> Result<String, Box<dyn Error>>{
-        Ok("".to_string())
+        let mut img = self.input_img.clone();
+        //printgrid(&img);
+        for _i in 0..50 {
+            img = step(&self.conv, &img);
+            //println!("done {}", i);
+        }
+        if img.default {
+            return Err(new_err("infinite result"));
+        }
+        let res = img.known.values().filter(|x|**x).count();
+        Ok(res.to_string())
     }
 }
