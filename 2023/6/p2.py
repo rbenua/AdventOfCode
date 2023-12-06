d36648523f95e6dbc3f87b9cb43ffe4d86461d47
Rick Benua <rick@benua.net>
2023-12-05 21:41:48 -0800

    day 6

---
+++ 2023/6/p2.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+from collections import deque
+from collections import defaultdict
+
+f = open(sys.argv[1], "r")
+
+def nums(s):
+    return [int(x) for x in re.findall(r'(-?\d+).?', s)]
+
+def nbrs(p, mx, my):
+    x, y = p
+    all = [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]
+    return [(x, y) for (x, y) in all if 0 <= x < mx and 0 <= y < my]
+
+
+def mdist(a, b):
+    return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
+def padd(a, b):
+    return (a[0] + b[0], a[1] + b[1])
+
+def pmul(a, n):
+    return (a[0] * n, a[1] * n)
+
+def pmod(p, mx, my):
+    return (p[0] % mx, p[1] % my)
+
+def cnbrs(c):
+    x, y, z = c
+    return [(x+1,y,z), (x-1,y,z), (x,y+1,z), (x,y-1,z), (x,y,z+1), (x,y,z-1)]
+
+time = int("".join(f.readline().split()[1:]))
+dist = int("".join(f.readline().split()[1:]))
+
+def test(i):
+    return i * (time - i) > dist
+
+def search(min, max, t):
+    if min == max:
+        return min
+    mid = (min + max) // 2
+    if t(mid):
+        return search(min, mid, t)
+    else:
+        return search(mid + 1, max, t)
+
+lower = search(0, time, test)
+upper = search(0, time, lambda i: not test(i))
+
+print(upper - lower)
