be3f33c577f9a58c11c63dbc78001f0106ae2664
Rick Benua <rick@benua.net>
2024-12-03 11:22:01 -0800

    2024 days 1-3

---
+++ 2024/2/p1.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+from collections import deque
+from collections import defaultdict
+
+f = open(sys.argv[1], "r")
+
+def nums(s):
+    return [int(x) for x in re.findall(r'(-?\d+).?', s)]
+
+def nbrs(p, mx, my):
+    x, y = p
+    all = [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]
+    return [(x, y) for (x, y) in all if 0 <= x < mx and 0 <= y < my]
+
+up = (0, -1)
+down = (0, 1)
+left = (-1, 0)
+right = (1, 0)
+
+def mdist(a, b):
+    return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
+def padd(a, b):
+    return (a[0] + b[0], a[1] + b[1])
+
+def pmul(a, n):
+    return (a[0] * n, a[1] * n)
+
+def pmod(p, mx, my):
+    return (p[0] % mx, p[1] % my)
+
+def cnbrs(c):
+    x, y, z = c
+    return [(x+1,y,z), (x-1,y,z), (x,y+1,z), (x,y-1,z), (x,y,z+1), (x,y,z-1)]
+
+def safe(level):
+    decreasing = level[1] < level[0]
+    for i in range(1, len(level)):
+        diff = level[i] - level[i-1]
+        if decreasing:
+            diff *= -1
+        if diff < 1 or diff > 3:
+            return False
+    return True
+
+print(sum(1 for line in f if safe(nums(line))))
