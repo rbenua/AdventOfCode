3392a9eeb0f8763f101c5a79ee154a81f81ae451
Rick Benua <rick@benua.net>
2024-12-06 21:35:29 -0800

    day 7

---
+++ 2024/7/p2.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+from collections import deque
+from collections import defaultdict
+
+f = open(sys.argv[1], "r")
+
+def nums(s):
+    return [int(x) for x in re.findall(r'(-?\d+).?', s)]
+
+def nbrs(p, mx, my):
+    x, y = p
+    all = [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]
+    return [(x, y) for (x, y) in all if 0 <= x < mx and 0 <= y < my]
+
+up = (0, -1)
+down = (0, 1)
+left = (-1, 0)
+right = (1, 0)
+
+def mdist(a, b):
+    return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
+def padd(a, b):
+    return (a[0] + b[0], a[1] + b[1])
+
+def pmul(a, n):
+    return (a[0] * n, a[1] * n)
+
+def pmod(p, mx, my):
+    return (p[0] % mx, p[1] % my)
+
+def cnbrs(c):
+    x, y, z = c
+    return [(x+1,y,z), (x-1,y,z), (x,y+1,z), (x,y-1,z), (x,y,z+1), (x,y,z-1)]
+
+def clamp(p, mx, my):
+    return (min(mx - 1, max(p[0], 0)), min(my - 1, max(p[1], 0)))
+
+def attempt(result, acc, rest):
+    if len(rest) == 0:
+        return result == acc
+    if acc > result:
+        return False
+    next = rest[0]
+    return attempt(result, acc * next, rest[1:]) \
+        or attempt(result, acc + next, rest[1:]) \
+        or attempt(result, int(str(acc) + str(next)), rest[1:])
+
+
+total = 0
+for line in f:
+    ns = nums(line)
+    result = ns.pop(0)
+    first = ns.pop(0)
+    if attempt(result, first, ns):
+        total += result
+print(total)
