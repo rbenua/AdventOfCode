45d7b64a009d1bc48618325583f9622d645443e9
Rick Benua <rick@benua.net>
2025-01-01 22:50:30 -0500

    some miscellaneous test files

---
+++ 2024/19/regex_test.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+from collections import deque
+from collections import defaultdict
+from functools import cache
+
+import signal
+
+f = open(sys.argv[1], "r")
+
+def nums(s):
+    return [int(x) for x in re.findall(r'(-?\d+).?', s)]
+
+def nbrs(p, mx, my):
+    x, y = p
+    all = [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]
+    return [(x, y) for (x, y) in all if 0 <= x < mx and 0 <= y < my]
+
+up = (0, -1)
+down = (0, 1)
+left = (-1, 0)
+right = (1, 0)
+dirs = [up, right, down, left]
+
+def turn(dir, right):
+    delta = 1 if right else -1
+    return dirs[(dirs.index(dir) + delta) % 4]
+
+def mdist(a, b):
+    return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
+def padd(a, b):
+    return (a[0] + b[0], a[1] + b[1])
+
+def psub(a, b):
+    return (a[0] - b[0], a[1] - b[1])
+
+def pmul(a, n):
+    return (a[0] * n, a[1] * n)
+
+def pmod(p, mx, my):
+    return (p[0] % mx, p[1] % my)
+
+def cnbrs(c):
+    x, y, z = c
+    return [(x+1,y,z), (x-1,y,z), (x,y+1,z), (x,y-1,z), (x,y,z+1), (x,y,z-1)]
+
+def clamp(p, mx, my):
+    return (min(mx - 1, max(p[0], 0)), min(my - 1, max(p[1], 0)))
+
+towels = f.readline().strip().split(", ")
+f.readline()
+patterns = f.read().strip().split("\n")
+
+def handler(signum, frame):
+    raise OSError("timed out")
+
+signal.signal(signal.SIGALRM, handler)
+
+pattern = patterns[0]
+completes = []
+
+for i in range(len(towels)):
+    to_remove = towels[i]
+    ts = towels.copy()
+    del ts[i]
+    p = re.compile(f"^({'|'.join(ts)})*$")
+    print(f"removing {to_remove}...")
+    signal.alarm(1)
+    try:
+        res = p.match(pattern)
+        completes.append(to_remove)
+        if res:
+            print("match!")
+        else:
+            print("no match!")
+    except OSError:
+        print("timeout!")
+    signal.alarm(0)
+print(completes)
