36679f64d2ddcc5c9d9015999895b80b860d5ee8
Rick Benua <rick@benua.net>
2025-12-05 00:32:41 -0800

    day 5 p2 better

--- 2025/5/p2.hs
+++ 2025/5/p2.hs
@@ -11,19 +11,11 @@ import Data.List.Split
 main = do
     args <- getArgs
     contents <- readFile (head args) 
-    let [ranges, vals] = splitOn "\n\n" contents
+    let [ranges, _] = splitOn "\n\n" contents
         iranges :: [[Integer]] = map (map read . splitOn "-") $ lines ranges
-        ivals :: [Integer] = map read $ lines vals
-        sranges = sortOn head iranges
-    print $ total sranges
+        [l,u]:sranges = sortOn head iranges
+    print $ total l u sranges
 
-overlaps upper [] = (upper, [])
-overlaps upper ([l, u]:rest) | l <= upper = overlaps (max u upper) rest
-overlaps upper all = (upper, all)
-
-total [] = 0
-total ([lower, upper]:rest) =
-    let 
-        (curr_end, remaining) = overlaps upper rest
-        curr = curr_end - lower + 1
-    in curr + total remaining
+total lower upper [] = upper - lower + 1
+total lower upper ([l, u]:rest) | l <= upper = total lower (max u upper) rest
+total lower upper ([l, u]:rest) = upper - lower + 1 + total l u rest
